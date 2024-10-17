31a844e8f44d88733e0358562ab6a8582e91e5aa
ignaciojavier27 <nacholin124@gmail.com>
2024-10-17 14:31:06 -0300

    Ubicacion Usuario

--- src/helpers/obtenerUbicacionUsuario.js
+++ src/helpers/obtenerUbicacionUsuario.js
@@ -5,13 +5,11 @@ export const obtenerUbicacionUsuario = (setUserPosition, defaultPosition) => {
           const { latitude, longitude } = position.coords;
           setUserPosition([latitude, longitude]);
         },
-        (error) => {
-          console.log('Error al obtener la ubicación del usuario:', error);
+        () => {
           setUserPosition(defaultPosition);
         }
       );
     } else {
-      console.log('Geolocalización no es compatible con este navegador');
       setUserPosition(defaultPosition);
     }
 };
